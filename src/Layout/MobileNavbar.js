0f6d1e3397810484e9971b4e1fd4ae2adbb42a4b
rahulrajsbkk <rahulrajsbkk@gmail.com>
2020-08-03 07:15:39 +0530

    Guest Navbar

--- src/Layout/MobileNavbar.js
+++ src/Layout/MobileNavbar.js
@@ -1,11 +1,33 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import Axios from 'axios';
+import { Link, useHistory } from 'react-router-dom';
 import { BankContext } from '../context/Context';
-import { Link } from 'react-router-dom';
+import LoginWrapper from '../components/LoginModal/LoginWrapper';
 
 function MobileNavbar({ active }) {
   const [navOpen, setNavOpen] = useState(false);
-  const { username, name, profileImg, login } = useContext(BankContext);
-  console.log('active :>> ', active);
+  const { username, name, profileImg, login, email } = useContext(BankContext);
+  const [countryName, setCountryName] = useState('');
+  useEffect(() => {
+    Axios.get('https://ipapi.co/country_name/')
+      .then((response) => {
+        let data = response.data;
+        setCountryName(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const history = useHistory();
+  const [loginModalOpen, setLoginModalOpen] = useState(false);
+  const [onLoginPage, setOnLoginPage] = useState('');
+  const onLogin = () => {
+    if (onLoginPage) {
+      history.push(onLoginPage);
+    }
+  };
+
   let title;
   switch (active) {
     case 'index':
@@ -27,71 +49,105 @@ function MobileNavbar({ active }) {
       break;
   }
   return (
-    <div className="mobile-navbar">
-      <div
-        onClick={() => setNavOpen(!navOpen)}
-        className={
-          'd-flex hamburger hamburger--squeeze' + (navOpen ? ' is-active' : '')
-        }
-      >
-        <span className="hamburger-box m-auto">
-          <span className="hamburger-inner"></span>
-        </span>
-      </div>
-      <div className="title">{title}</div>
-      {navOpen ? (
-        <div className="menu-open">
-          <div className="profile">
-            <img
-              src={
-                profileImg
-                  ? profileImg
-                  : `https://api.adorable.io/avatars/100/${username}.png`
-              }
-              alt=""
-            />
-            <div className="user-info">
-              <div className="name">{name ? name : username}&nbsp;</div>
-              <div className="balance">$23,367.32</div>
-            </div>
-          </div>
-          <div className="menu">
-            <Link to="/" className={`menu-itm ${active === 'index'}`}>
-              Index
-            </Link>
-            <Link
-              to="/portfolio"
-              className={`menu-itm ${active === 'portfolio'}`}
-            >
-              Portfolio
-            </Link>
-            <Link
-              to="/transactions"
-              className={`menu-itm ${active === 'transactions-undefined'}`}
-            >
-              Transactions
-            </Link>
-            <Link
-              to="/transactions/deposit"
-              className={`menu-itm ${active === 'transactions-deposit'}`}
-            >
-              Deposit
-            </Link>
-            <Link
-              to="/transactions/withdraw"
-              className={`menu-itm ${active === 'transactions-withdraw'}`}
-            >
-              Withdraw
-            </Link>
-            <div className="logout-btn" onClick={() => login()}>
-              Logout
-            </div>
-          </div>
+    <>
+      <div className="mobile-navbar">
+        <div
+          onClick={() => setNavOpen(!navOpen)}
+          className={
+            'd-flex hamburger hamburger--squeeze' +
+            (navOpen ? ' is-active' : '')
+          }
+        >
+          <span className="hamburger-box m-auto">
+            <span className="hamburger-inner"></span>
+          </span>
         </div>
+        <div className="title">{title}</div>
+        {navOpen ? (
+          <div className="menu-open">
+            <div className="profile">
+              <img
+                src={
+                  profileImg
+                    ? profileImg
+                    : `https://api.adorable.io/avatars/100/${username}.png`
+                }
+                alt=""
+              />
+              <div className="user-info">
+                <div className="name">
+                  {email ? (name ? name : username) : countryName}&nbsp;
+                </div>
+                <div className="balance">{email ? '$23, 367.32' : 'Guest'}</div>
+              </div>
+            </div>
+            <div className="menu">
+              <Link to="/" className={`menu-itm ${active === 'index'}`}>
+                Index
+              </Link>
+              <Link
+                to="/portfolio"
+                className={`menu-itm ${active === 'portfolio'}`}
+              >
+                Portfolio
+              </Link>
+              <Link
+                to="/transactions"
+                className={`menu-itm ${active === 'transactions-undefined'}`}
+              >
+                Transactions
+              </Link>
+              <Link
+                to="/transactions/deposit"
+                className={`menu-itm ${active === 'transactions-deposit'}`}
+              >
+                Deposit
+              </Link>
+              <Link
+                to="/transactions/withdraw"
+                className={`menu-itm ${active === 'transactions-withdraw'}`}
+              >
+                Withdraw
+              </Link>
+              {email ? (
+                <div
+                  className="logout-btn"
+                  role="button"
+                  tabIndex="0"
+                  onClick={() => login()}
+                >
+                  Logout
+                </div>
+              ) : (
+                <div
+                  className="logout-btn"
+                  onClick={() => {
+                    setLoginModalOpen(true);
+                  }}
+                  role="button"
+                  tabIndex="0"
+                >
+                  Get Started
+                </div>
+              )}
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
+      {loginModalOpen ? (
+        <LoginWrapper
+          onClose={() => {
+            setLoginModalOpen(false);
+            setOnLoginPage();
+          }}
+          onLogin={onLogin}
+        />
       ) : (
         ''
       )}
-    </div>
+    </>
   );
 }
 
