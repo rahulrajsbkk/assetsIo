2c245168c56f74f365670f00f3a4d17b0d360f3e
rahulrajsbkk <rahulrajsbkk@gmail.com>
2020-08-04 07:27:15 +0530

    Search Filter Added

--- src/components/MarketPage/EarnIntrest/AssetPlatformTable.js
+++ src/components/MarketPage/EarnIntrest/AssetPlatformTable.js
@@ -16,6 +16,7 @@ function AssetPlatformTable({ coinList, searchTitle }) {
   const [dropDownOpen, setDropDownOpen] = useState(false);
   const [fullScreen, setFullScreen] = useState(false);
   const [coinSelect, setCoinSelect] = useState({});
+  const [search, setSearch] = useState('');
   useEffect(() => {
     if (coinList[0]) setCoinSelect(coinList[0]);
   }, [coinList]);
@@ -59,8 +60,37 @@ function AssetPlatformTable({ coinList, searchTitle }) {
           </div>
         </div>
         <label className="searchWrapper">
-          <input type="text" placeholder={`Search ${searchTitle}`} />
+          <input
+            type="text"
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder={`Search ${searchTitle}`}
+          />
           <FontAwesomeIcon icon={faSearch} />
+          {search ? (
+            <div className="menu">
+              {coinList
+                .filter(
+                  (coin) =>
+                    coin.coinName
+                      .toLowerCase()
+                      .includes(search.toLowerCase()) ||
+                    coin.coinSymbol.toLowerCase().includes(search.toLowerCase())
+                )
+                .map((coin) => (
+                  <div
+                    key={coin._id}
+                    className="btn-togle"
+                    onClick={() => setCoinSelect(coin)}
+                  >
+                    <img src={coin.coinImage} alt="" />
+                    {coin.coinName}
+                    <span className="platform">{coin.coinSymbol}</span>
+                  </div>
+                ))}
+            </div>
+          ) : (
+            ''
+          )}
         </label>
         <img
           onClick={() => setFullScreen(!fullScreen)}
--- src/components/MarketPage/EarnIntrest/EarnIntrest.js
+++ src/components/MarketPage/EarnIntrest/EarnIntrest.js
@@ -21,6 +21,7 @@ function EarnIntrest({ title }) {
   const [filterList, setFilterList] = useState([]);
   const [searchTitle, setSearchTitle] = useState('Stable Coins');
   const [coinSelect, setCoinSelect] = useState({});
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     switch (assetTab) {
@@ -87,9 +88,41 @@ function EarnIntrest({ title }) {
             </div>
           </div>
           <label className="searchWrapper">
-            <input type="text" placeholder={`Search ${searchTitle}`} />
+            <input
+              type="text"
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder={`Search ${searchTitle}`}
+            />
             <FontAwesomeIcon icon={faSearch} />
+            {search ? (
+              <div className="menu">
+                {coinList
+                  .filter(
+                    (coin) =>
+                      coin.coinName
+                        .toLowerCase()
+                        .includes(search.toLowerCase()) ||
+                      coin.coinSymbol
+                        .toLowerCase()
+                        .includes(search.toLowerCase())
+                  )
+                  .map((coin) => (
+                    <div
+                      key={coin._id}
+                      className="btn-togle"
+                      onClick={() => setCoinSelect(coin)}
+                    >
+                      <img src={coin.coinImage} alt="" />
+                      {coin.coinName}
+                      <span className="platform">{coin.coinSymbol}</span>
+                    </div>
+                  ))}
+              </div>
+            ) : (
+              ''
+            )}
           </label>
+
           <img
             onClick={() => setFullScreen(!fullScreen)}
             className="fullIcon"
