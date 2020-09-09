5da472d6ef032dc2ef7372071589671205807852
rahulrajsbkk <rahulrajsbkk@gmail.com>
2020-09-09 06:24:46 +0530

    Custom Right Click On Charts Card

--- src/components/NetWorthPage/NetWorthChart.js
+++ src/components/NetWorthPage/NetWorthChart.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
-
+import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu';
 import { FormatCurrency } from '../../utils/FunctionTools';
 import DonutChart from '../DonutChart/Index';
 
@@ -24,6 +24,11 @@ function NetWorthChart() {
   const { coinNameObject } = useContext(BankContext);
 
   const [title, setTitle] = useState('Networth By Asset Class');
+
+  function handleClick(e, data) {
+    console.log(data.foo);
+  }
+
   useEffect(() => {
     if (liquidity) {
       if (liquidity === 'Liquid') {
@@ -53,83 +58,97 @@ function NetWorthChart() {
   }, [assetClass, assetCoin, liquidity]);
 
   return (
-    <div className="chartSection">
-      <div className="breadCrumbs d-flex">
-        <span
-          onClick={() => {
-            setAssetClass(null);
-            setAssetCoin(null);
-            setLiquidity(null);
-          }}
-        >
-          Net-Worth
-        </span>
-        {assetClass ? (
-          <>
-            &gt;
-            <span
-              onClick={() => {
-                setAssetCoin(null);
-                setLiquidity(null);
-              }}
-            >
-              {assetClass}
-            </span>
-          </>
-        ) : (
-          <></>
-        )}
-        {assetCoin ? (
-          <>
-            &gt;
-            <span
-              onClick={() => {
-                setLiquidity(null);
-              }}
-            >
-              {assetCoin}
-            </span>
-          </>
-        ) : (
-          <></>
-        )}
-        {liquidity ? (
-          <>
-            &gt;
-            <span onClick={() => {}}>{liquidity}</span>
-          </>
-        ) : (
-          <></>
-        )}
-      </div>
-      <div role="button" tabIndex="0" className="total mt-auto">
-        <h6>{loadingAppBalance ? <Skeleton width="50%" /> : title}</h6>
-        <h4>
-          {loadingAppBalance ? (
-            <Skeleton width="70%" />
+    <>
+      <ContextMenuTrigger className="chartSection" id="same_unique_identifier">
+        <div className="breadCrumbs d-flex">
+          <span
+            onClick={() => {
+              setAssetClass(null);
+              setAssetCoin(null);
+              setLiquidity(null);
+            }}
+          >
+            Net-Worth
+          </span>
+          {assetClass ? (
+            <>
+              &gt;
+              <span
+                onClick={() => {
+                  setAssetCoin(null);
+                  setLiquidity(null);
+                }}
+              >
+                {assetClass}
+              </span>
+            </>
           ) : (
-            `$${FormatCurrency(selectedTotalBalance)}`
+            <></>
           )}
-        </h4>
-      </div>
-      <div className="chart-section mb-auto">
-        <div className="chart">
-          <div className="chartLogo">
-            <img src={assetLogo} alt="" />
-          </div>
-          <DonutChart
-            pieData={cardList}
-            onMouseOver={(segmentIndex) => {
-              setSegment(segmentIndex);
-            }}
-            onMouseOut={() => {
-              setSegment(null);
-            }}
-            segment={segment}
-          />
+          {assetCoin ? (
+            <>
+              &gt;
+              <span
+                onClick={() => {
+                  setLiquidity(null);
+                }}
+              >
+                {assetCoin}
+              </span>
+            </>
+          ) : (
+            <></>
+          )}
+          {liquidity ? (
+            <>
+              &gt;
+              <span onClick={() => {}}>{liquidity}</span>
+            </>
+          ) : (
+            <></>
+          )}
         </div>
-      </div>
-    </div>
+        <div role="button" tabIndex="0" className="total mt-auto">
+          <h6>{loadingAppBalance ? <Skeleton width="50%" /> : title}</h6>
+          <h4>
+            {loadingAppBalance ? (
+              <Skeleton width="70%" />
+            ) : (
+              `$${FormatCurrency(selectedTotalBalance)}`
+            )}
+          </h4>
+        </div>
+        <div className="chart-section mb-auto">
+          <div className="chart">
+            <div className="chartLogo">
+              <img src={assetLogo} alt="" />
+            </div>
+            <DonutChart
+              pieData={cardList}
+              onMouseOver={(segmentIndex) => {
+                setSegment(segmentIndex);
+              }}
+              onMouseOut={() => {
+                setSegment(null);
+              }}
+              segment={segment}
+            />
+          </div>
+        </div>
+      </ContextMenuTrigger>
+
+      <ContextMenu id="same_unique_identifier">
+        <MenuItem data={{ foo: 'bar' }} onClick={handleClick}>
+          Open In Current Tab
+        </MenuItem>
+        <MenuItem data={{ foo: 'bar' }} onClick={handleClick}>
+          Open In New Assets Tab
+        </MenuItem>
+        <MenuItem data={{ foo: 'bar' }} onClick={handleClick}>
+          Open In New Browser Tab
+        </MenuItem>
+      </ContextMenu>
+    </>
   );
 }
 
