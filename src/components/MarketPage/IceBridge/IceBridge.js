be705dd86751fb8a3b1832ada088b23708b4adae
rahulrajsbkk <rahulrajsbkk@gmail.com>
2020-09-21 03:26:00 +0530

    Ice Bridge Page Complete

--- src/components/MarketPage/IceBridge/IceBridge.js
+++ src/components/MarketPage/IceBridge/IceBridge.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import assetsLogo from '../../../static/images/assetsLogo.svg';
+import Scrollbars from 'react-custom-scrollbars';
+
+import compound from '../../../static/images/marketLogos/compound.svg';
+import curve from '../../../static/images/marketLogos/curve.svg';
+import maker from '../../../static/images/marketLogos/maker.svg';
+import mortage from '../../../static/images/marketLogos/mortage.svg';
+import uniswap from '../../../static/images/marketLogos/uniswap.svg';
 
 function IceBridge() {
   return (
@@ -17,6 +24,74 @@ function IceBridge() {
       </p>
       <div className="whatBridgeSupport">
         <div className="title">What Doest The Bridge Support? </div>
+        <div className="listsContainer">
+          <Scrollbars
+            className="scrollList"
+            renderThumbHorizontal={() => <div />}
+            renderThumbVertical={() => <div />}
+            renderView={(props) => (
+              <div {...props} className="scrollListView" />
+            )}
+          >
+            <div className="head">Blockchain Based Protocols</div>
+            <div
+              className="marketCard"
+              style={{
+                background: '#1AAB9A',
+                color: 'white',
+              }}
+            >
+              <img src={maker} alt="" />
+              MakerDAO
+            </div>
+            <div
+              className="marketCard"
+              style={{
+                background: '#0AD394',
+                color: 'white',
+              }}
+            >
+              <img src={compound} alt="" />
+              Compound
+            </div>
+            <div
+              className="marketCard"
+              style={{
+                background: '#DC6BE5',
+                color: 'white',
+              }}
+            >
+              <img src={uniswap} alt="" />
+              Uniswap
+            </div>
+            <div
+              className="marketCard"
+              style={{
+                background: '#3465A4',
+                color: 'white',
+              }}
+            >
+              <img src={curve} alt="" />
+              Curve
+            </div>
+          </Scrollbars>
+          <Scrollbars
+            className="scrollList"
+            renderThumbHorizontal={() => <div />}
+            renderThumbVertical={() => <div />}
+            renderView={(props) => (
+              <div {...props} className="scrollListView" />
+            )}
+          >
+            <div className="head">
+              Traditional Debt Instruments (Coming Soon)
+            </div>
+            <div className="marketCard">
+              <img src={mortage} alt="" />
+              Mortage
+            </div>
+          </Scrollbars>
+        </div>
       </div>
     </div>
   );
