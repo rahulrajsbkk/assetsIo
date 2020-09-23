bdff16fb4a846d26c619aaef2575fd1dd4a22cfc
rahulrajsbkk <rahulrajsbkk@gmail.com>
2020-09-23 08:00:59 +0530

    Ice Trust Mobile View First Page

--- src/components/MarketPage/IceTrust/IceTrust.js
+++ src/components/MarketPage/IceTrust/IceTrust.js
@@ -8,106 +8,116 @@ import maker from '../../../static/images/marketLogos/maker.svg';
 import mortage from '../../../static/images/marketLogos/mortage.svg';
 import uniswap from '../../../static/images/marketLogos/uniswap.svg';
 import CreateDefiTrustWrapper from '../../CreateDefiTrust/CreateDefiTrustWrapper';
+import useWindowDimensions from '../../../utils/WindowSize';
+import IceTrustMobile from './IceTrustMobile';
 
 function IceTrust() {
   const [openDefiLogin, setOpenDefiLogin] = useState(false);
+  const { width } = useWindowDimensions();
   return (
     <>
-      <div className="iceTrust">
-        <div className="heading">
-          <img src={assetsLogo} alt="" />
-          DeFan Trusts
-        </div>
-        <p className="iceDesc">
-          A Defan other wise known as Decentralized Financial Assets Network
-          Trust Is A Programmticaly Controlled Storge Which Allows You To
-          Transition Between CDP Smart Contracts & Traditional Debt Instruments.
-          You Can Leverage The Bridge Inside A DEFI Assets Trust. Anyone Can
-          Setup A Free Defi Assets Trust Where They Can Manage, Convert, &
-          Leverage Their DEFI & Traditional Debt Instruments.
-        </p>
-        <div className="whatBridgeSupport">
-          <div className="title">What Doest The Bridge Support? </div>
-          <div className="listsContainer">
-            <Scrollbars
-              className="scrollList"
-              renderThumbHorizontal={() => <div />}
-              renderThumbVertical={() => <div />}
-              renderView={(props) => (
-                <div {...props} className="scrollListView" />
-              )}
-            >
-              <div className="head">Blockchain Based Protocols</div>
-              <div
-                className="marketCard"
-                style={{
-                  background: '#1AAB9A',
-                  color: 'white',
-                }}
-              >
-                <img src={maker} alt="" />
-                MakerDAO
+      {width > 768 ? (
+        <>
+          <div className="iceTrust">
+            <div className="heading">
+              <img src={assetsLogo} alt="" />
+              DeFan Trusts
+            </div>
+            <p className="iceDesc">
+              A Defan other wise known as Decentralized Financial Assets Network
+              Trust Is A Programmticaly Controlled Storge Which Allows You To
+              Transition Between CDP Smart Contracts & Traditional Debt
+              Instruments. You Can Leverage The Bridge Inside A DEFI Assets
+              Trust. Anyone Can Setup A Free Defi Assets Trust Where They Can
+              Manage, Convert, & Leverage Their DEFI & Traditional Debt
+              Instruments.
+            </p>
+            <div className="whatBridgeSupport">
+              <div className="title">What Doest The Bridge Support? </div>
+              <div className="listsContainer">
+                <Scrollbars
+                  className="scrollList"
+                  renderThumbHorizontal={() => <div />}
+                  renderThumbVertical={() => <div />}
+                  renderView={(props) => (
+                    <div {...props} className="scrollListView" />
+                  )}
+                >
+                  <div className="head">Blockchain Based Protocols</div>
+                  <div
+                    className="marketCard"
+                    style={{
+                      background: '#1AAB9A',
+                      color: 'white',
+                    }}
+                  >
+                    <img src={maker} alt="" />
+                    MakerDAO
+                  </div>
+                  <div
+                    className="marketCard"
+                    style={{
+                      background: '#0AD394',
+                      color: 'white',
+                    }}
+                  >
+                    <img src={compound} alt="" />
+                    Compound
+                  </div>
+                  <div
+                    className="marketCard"
+                    style={{
+                      background: '#DC6BE5',
+                      color: 'white',
+                    }}
+                  >
+                    <img src={uniswap} alt="" />
+                    Uniswap
+                  </div>
+                  <div
+                    className="marketCard"
+                    style={{
+                      background: '#3465A4',
+                      color: 'white',
+                    }}
+                  >
+                    <img src={curve} alt="" />
+                    Curve
+                  </div>
+                </Scrollbars>
+                <Scrollbars
+                  className="scrollList"
+                  renderThumbHorizontal={() => <div />}
+                  renderThumbVertical={() => <div />}
+                  renderView={(props) => (
+                    <div {...props} className="scrollListView" />
+                  )}
+                >
+                  <div className="head">
+                    Traditional Debt Instruments (Coming Soon)
+                  </div>
+                  <div className="marketCard">
+                    <img src={mortage} alt="" />
+                    Mortage
+                  </div>
+                </Scrollbars>
               </div>
-              <div
-                className="marketCard"
-                style={{
-                  background: '#0AD394',
-                  color: 'white',
-                }}
-              >
-                <img src={compound} alt="" />
-                Compound
-              </div>
-              <div
-                className="marketCard"
-                style={{
-                  background: '#DC6BE5',
-                  color: 'white',
-                }}
-              >
-                <img src={uniswap} alt="" />
-                Uniswap
-              </div>
-              <div
-                className="marketCard"
-                style={{
-                  background: '#3465A4',
-                  color: 'white',
-                }}
-              >
-                <img src={curve} alt="" />
-                Curve
-              </div>
-            </Scrollbars>
-            <Scrollbars
-              className="scrollList"
-              renderThumbHorizontal={() => <div />}
-              renderThumbVertical={() => <div />}
-              renderView={(props) => (
-                <div {...props} className="scrollListView" />
-              )}
-            >
-              <div className="head">
-                Traditional Debt Instruments (Coming Soon)
-              </div>
-              <div className="marketCard">
-                <img src={mortage} alt="" />
-                Mortage
-              </div>
-            </Scrollbars>
+            </div>
           </div>
-        </div>
-      </div>
-      <div
-        className="footer-main bridge-footer"
-        onClick={() => setOpenDefiLogin(true)}
-      >
-        Create DEFI Assets Trust
-      </div>
-      {openDefiLogin ? (
-        <CreateDefiTrustWrapper setOpenDefiLogin={setOpenDefiLogin} />
+          <div
+            className="footer-main bridge-footer"
+            onClick={() => setOpenDefiLogin(true)}
+          >
+            Create DEFI Assets Trust
+          </div>
+          {openDefiLogin ? (
+            <CreateDefiTrustWrapper setOpenDefiLogin={setOpenDefiLogin} />
+          ) : (
+            ''
+          )}
+        </>
       ) : (
-        ''
+        <IceTrustMobile />
       )}
     </>
   );
---
+++ src/components/MarketPage/IceTrust/IceTrustMobile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function IceTrustMobile() {
+  return (
+    <div className="iceTrustMobile">
+      <h3>DEFAN</h3>
+      <h5>Trusts</h5>
+      <p>
+        A Defan (Decentralized Financial Assets Network) Trust Is A
+        Programmticaly Controlled Storge Which Allows You To Transition Between
+        CDP Smart Contracts & Traditional Debt Instruments. You Can Leverage The
+        Bridge Inside A DEFI Assets Trust.{' '}
+      </p>
+      <p>
+        Anyone Can Setup A Free Defi Assets Trust Where They Can Manage,
+        Convert, & Leverage Their DEFI & Traditional Debt Instruments.
+      </p>
+      <div className="btnLearnMore">Learn More</div>
+    </div>
+  );
+}
+
+export default IceTrustMobile;
